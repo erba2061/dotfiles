bf68891854fc735ce9ec4967cb3042b7e9e9ab22
Erikas Baronas <erikas.baronas@surfsharkteam.com>
2024-12-21 19:41:11 +0200

    upd

--- nvim/init.lua
+++ nvim/init.lua
@@ -690,10 +690,11 @@ require("lazy").setup({
 				--
 				-- You can use a sub-list to tell conform to run *until* a formatter
 				-- is found.
-				javascript = { { "prettierd", "prettier" } },
-				javascriptreact = { { "prettierd", "prettier" } },
-				less = { { "prettierd", "prettier" } },
-				templ = { { "templ" } },
+				javascript = { "prettierd", "prettier" },
+				typescript = { "prettierd", "prettier" },
+				javascriptreact = { "prettierd", "prettier" },
+				less = { "prettierd", "prettier" },
+				templ = { "templ" },
 			},
 		},
 	},
